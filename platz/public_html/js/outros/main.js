31983230c997fc834f238465eecd5ea75490ec79
15153770 <15153770@LAB10-02.suicobrasileira.sp>
2016-09-23 16:36:18 -0300

    deletar copia do projeto existente dentro do projeto

--- platz/public_html/js/outros/main.js
+++
@@ -1,7 +0,0 @@
-if (navigator.serviceWorker) {
-    navigator.serviceWorker.register('./sw.js').then(function () {
-        console.log('Funcionou');
-    }).catch(function () {
-        console.log('não funcionou');
-    })
-}
